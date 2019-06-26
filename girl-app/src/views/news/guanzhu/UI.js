f365f7b173e5b2e4a5ac1181ff03c6e82e7a686a
xcm1 <121969209@qq.com>
2019-06-26 20:50:41 +0800

    我的关注页面的修改

--- girl-app/src/views/news/guanzhu/UI.js
+++ girl-app/src/views/news/guanzhu/UI.js
@@ -22,13 +22,13 @@ class Com extends Component {
           <i className='iconfont icon-fanhui' onClick = {this.goBack.bind(this)}></i>
           <span>我的关注</span>
         </header>
-        <section className='content' >
+        <section className='content guanzhu' >
           <Tabs tabs={tabs}
             initialPage={0}
             // onChange={(tab, index) => { console.log('onChange', index, tab); }}
             // onTabClick={(tab, index) => { console.log('onTabClick', index, tab); }}
           >
-            <div style={{ display: 'flex',  height: '289px', backgroundColor: '#fff',margin:6 }}>
+            <div style={{  margin:6 }}>
               <div className='yonghu'>
                 <div className= 'yonghu-t'>
                   <i>你可能感兴趣的人</i>
@@ -39,8 +39,8 @@ class Com extends Component {
                       <li>
                         <div className= 'yonghu-c-l'>
                           <img src={require('@/views/news/news-img/touxiang.png')} alt="" />
-                          <div>
-                            <h3>小姐姐</h3>
+                          <div style= {{marginLeft:6}}>
+                            <h3>皮卡丘</h3>
                             <span>你可能感兴趣的人</span>
                           </div>
                         </div>
@@ -48,9 +48,9 @@ class Com extends Component {
                       </li>
                       <li>
                         <div className= 'yonghu-c-l'>
-                          <img src={require('@/views/news/news-img/touxiang.png')} alt="" />
-                          <div>
-                            <h3>小姐姐</h3>
+                          <img src={require('@/views/news/news-img/tou4.png')} alt="" />
+                          <div style= {{marginLeft:6}}>
+                            <h3>啦哇一</h3>
                             <span>你可能感兴趣的人</span>
                           </div>
                         </div>
@@ -58,9 +58,9 @@ class Com extends Component {
                       </li>
                       <li>
                         <div className= 'yonghu-c-l'>
-                          <img src={require('@/views/news/news-img/touxiang.png')} alt="" />
-                          <div>
-                            <h3>小姐姐</h3>
+                          <img src={require('@/views/news/news-img/tou2.png')} alt="" />
+                          <div style= {{marginLeft:6}}>
+                            <h3>暴龙兽妹妹</h3>
                             <span>你可能感兴趣的人</span>
                           </div>
                         </div>
@@ -68,6 +68,48 @@ class Com extends Component {
                       </li>
                     </ul>
                 </div>
+                         
+              </div>
+              <div className='yonghu'>
+                <div className= 'yonghu-t'>
+                  <i>我关注的好友</i>
+                  <span>更多...</span>
+                </div>
+                <div className= 'yonghu-c'>
+                    <ul>
+                      <li>
+                        <div className= 'yonghu-c-l'>
+                          <img src={require('@/views/news/news-img/tou3.png')} alt="" />
+                          <div style= {{marginLeft:6}}>
+                            <h3>小姐姐</h3>
+                            <span>你认识的人</span>
+                          </div>
+                        </div>
+                        <div className='yonghu-c-r' style={{background:'#ec6b9b'}}>已关注</div>
+                      </li>
+                      <li>
+                        <div className= 'yonghu-c-l'>
+                          <img src={require('@/views/news/news-img/tou2.png')} alt="" />
+                          <div style= {{marginLeft:6}}>
+                            <h3>大姐姐</h3>
+                            <span>你认识的人</span>
+                          </div>
+                        </div>
+                        <div className='yonghu-c-r' style={{background:'#ec6b9b'}}>已关注</div>
+                      </li>
+                      <li>
+                        <div className= 'yonghu-c-l'>
+                          <img src={require('@/views/news/news-img/tou1.png')} alt="" />
+                          <div style= {{marginLeft:6}}>
+                            <h3>瑜伽</h3>
+                            <span>你认识的人</span>
+                          </div>
+                        </div>
+                        <div className='yonghu-c-r' style={{background:'#ec6b9b'}}>已关注</div>
+                      </li>
+                    </ul>
+                </div>
+                         
               </div>
             </div>
             <div style={{ display: 'flex',  height: '289px', backgroundColor: '#fff',margin:6  }}>
