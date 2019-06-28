d4dbb3f3490ce7edf92644c37484e9454dad542a
Omind-4 <15738353168@163.com>
2019-06-28 20:58:59 +0800

    首页详情页

--- girl-app/src/components/home/tj_prolist.js
+++ girl-app/src/components/home/tj_prolist.js
@@ -1,67 +1,72 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 
 const Com = () => (
-
-  <section className = "tj">
-  <div className = "tj_prolist" >
-    <img src = "images/img1.png" alt = "" className = "img_src"/>
-    <p>今日穿搭，很oversize。</p> 
-    <div className = "prolist_b">
-      <div className = "prolist_l">
-        <img alt = "" src = "images/t1.png" />
-        <span>zl0818</span>
-      </div>
-      <div className = "prolist_r">
-        <a href = "##" ><img alt = ""src = "images/scq.png" /></a>
-        <span>16</span>
-        <a href = "##"><img alt = ""src = "images/pl.png" /></a>
-        <span>7</span>
-        <a href = "##"><img alt = ""src = "images/dzq.png" /></a>
-        <span>1</span>
+      <div className = "box">
+      <div className = "content">
+         <section className = "tj">
+         <NavLink className = "tj_prolist" to='/detail/homedetail'>
+           <img src = "images/img1.png" alt = "" className = "img_src"/>
+           <p>今日穿搭，很oversize。</p> 
+           <div className = "prolist_b">
+             <div className = "prolist_l">
+               <img alt = "" src = "images/t1.png" />
+               <span>zl0818</span>
+             </div>
+             <div className = "prolist_r">
+               <a href = "##" ><img alt = ""src = "images/scq.png" /></a>
+               <span>16</span>
+               <a href = "##"><img alt = ""src = "images/pl.png" /></a>
+               <span>7</span>
+               <a href = "##"><img alt = ""src = "images/dzq.png" /></a>
+               <span>1</span>
+             </div>
+           </div>
+         </NavLink>
+      
+         <div className = "tj_prolist">
+           <img src = "images/img2.png" alt = "" className = "img_src"/>
+           <p>今日穿搭，很oversize。</p> 
+           <div className = "prolist_b">
+             <div className = "prolist_l">
+               <img alt = "" src = "images/t2.png" />
+               <span>zl0818</span>
+             </div>
+             <div className = "prolist_r">
+               <a href = "##" ><img alt = ""src = "images/scq.png" /></a>
+               <span>16</span>
+               <a href = "##"><img alt = ""src = "images/pl.png" /></a>
+               <span>7</span>
+               <a href = "##"><img alt = ""src = "images/dzq.png" /></a>
+               <span>1</span>
+             </div>
+           </div>
+         </div>
+      
+      
+         <div className = "tj_prolist">
+           <img src = "images/img3.png" alt = "" className = "img_src"/>
+           <p>今日穿搭，很oversize。</p> 
+           <div className = "prolist_b">
+             <div className = "prolist_l">
+               <img alt = "" src = "images/t3.png" />
+               <span>zl0818</span>
+             </div>
+             <div className = "prolist_r">
+               <a href = "##" ><img alt = ""src = "images/scq.png" /></a>
+               <span>16</span>
+               <a href = "##"><img alt = ""src = "images/pl.png" /></a>
+               <span>7</span>
+               <a href = "##"><img alt = ""src = "images/dzq.png" /></a>
+               <span>1</span>
+             </div>
+           </div>
+         </div>
+       </section> 
+        
       </div>
     </div>
-  </div>
-
-  <div className = "tj_prolist">
-    <img src = "images/img2.png" alt = "" className = "img_src"/>
-    <p>今日穿搭，很oversize。</p> 
-    <div className = "prolist_b">
-      <div className = "prolist_l">
-        <img alt = "" src = "images/t2.png" />
-        <span>zl0818</span>
-      </div>
-      <div className = "prolist_r">
-        <a href = "##" ><img alt = ""src = "images/scq.png" /></a>
-        <span>16</span>
-        <a href = "##"><img alt = ""src = "images/pl.png" /></a>
-        <span>7</span>
-        <a href = "##"><img alt = ""src = "images/dzq.png" /></a>
-        <span>1</span>
-      </div>
-    </div>
-  </div>
-
-
-  <div className = "tj_prolist">
-    <img src = "images/img3.png" alt = "" className = "img_src"/>
-    <p>今日穿搭，很oversize。</p> 
-    <div className = "prolist_b">
-      <div className = "prolist_l">
-        <img alt = "" src = "images/t3.png" />
-        <span>zl0818</span>
-      </div>
-      <div className = "prolist_r">
-        <a href = "##" ><img alt = ""src = "images/scq.png" /></a>
-        <span>16</span>
-        <a href = "##"><img alt = ""src = "images/pl.png" /></a>
-        <span>7</span>
-        <a href = "##"><img alt = ""src = "images/dzq.png" /></a>
-        <span>1</span>
-      </div>
-    </div>
-  </div>
-</section> 
-)
+    )
 
 export default Com;
--- girl-app/src/layout/DetailApp.js
+++ girl-app/src/layout/DetailApp.js
@@ -4,10 +4,13 @@ import Nologin from '@/views/nologintel';
 import User from '@/views/user';
 import Code from '@/views/code';
 import Infodata from '@/views/infodata';
+import Homedetail from '@/views/homedetail';
 import { Switch, Route} from 'react-router-dom';
+
 
 const Com = () => (
   <Switch>
+    <Route path = '/detail/homedetail' component = { Homedetail }/>
     <Route path = '/detail/nologintel' component = { Nologin }/>
     <Route path = '/detail/Infodata' component = { Infodata }/>
     <Route path = '/detail/user' component = { User }/>
